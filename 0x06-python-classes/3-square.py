d40b0ef586722cc68c9251f9994cdcf2ddcd6eb9
Faith <faithmncube48@gmail.com>
2021-12-07 08:47:31 -0800

    3 square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python3
-"""A class Square that defines a square
-by: (based on 1-square.py)
+"""A class Square that defines a square by:
+(based on 2-square.py)
 """
 
 
 class Square:
-    """Square class with a private attribute
+    """Square class with a private attribute -
     size.
     """
 
     def __init__(self, size=0):
         """Initializes the size variable as a private
         instance artribute
-        Args:
-            __size (int): The __size of the new square.
         """
         self.__size = size
+
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
+
+    def area(self):
+        """Returns the current
+        square area
+        """
+        return (self.__size ** 2)
