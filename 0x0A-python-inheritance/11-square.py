5e46981af024e426083afe19a059848fe4653ab6
Faith <faithmncube48@gmail.com>
2021-12-20 10:54:55 -0800

    11 square

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -15,3 +15,6 @@ class Square(Rectangle):
     def area(self):
         '''get that number'''
         return self.__size ** 2
+
+    def __str__(self):
+        return f"[Square] {self.__size}/{self.__size}"
