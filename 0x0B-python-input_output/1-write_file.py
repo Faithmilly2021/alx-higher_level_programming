968e5a86cb929fa044c817310df29ac8733e5562
Faith <faithmncube48@gmail.com>
2021-12-21 09:34:17 -0800

    1 write file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a function that writes a string to a text file"""
+
+
+def write_file(filename="", text=""):
+    """Writes to a text file and return the number of
+         characters written
+    Args:
+        filename(str): The name of the file to write to
+        text(str): The text to write to the file.
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return (f.write(text))
