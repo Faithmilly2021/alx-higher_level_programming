fa72a98e3ed5487c57ff6263da96dbd62b43acf7
Faith <faithmncube48@gmail.com>
2021-12-21 10:14:23 -0800

    10 student

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Defines a Student class
+"""
+
+
+class Student:
+    """A class representing a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Instantiation
+        Args:
+            first_name (str): Student's first name
+            last_name (str): Student's last name
+            age (int): Student's age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representationn of a Student
+        If ``attrs`` is a list of strings, only attribute names contained
+        in this list must be retrieved. Else, all attributes
+        must be retrived.
+        Args:
+            attrs (list): Indicates specific attributes to represent
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {i: getattr(self, i) for i in attrs if hasattr(self, i)}
+        return self.__dict__
