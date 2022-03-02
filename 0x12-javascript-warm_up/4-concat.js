88ef6498368f9617f5ae4e513dd8400185013773
Faith <faithmncube48@gmail.com>
2022-03-02 01:40:33 -0800

    concat

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,2 +1,9 @@
 #!/usr/bin/node
-console.log(`${process.argv[2]} is ${process.argv[3]}`);
+const args = require('process').argv;
+let firstArg = args.slice(2, 3).toString();
+let secondArg = args.slice(3, 4).toString();
+
+if (!firstArg) firstArg = undefined;
+if (!secondArg) secondArg = undefined;
+
+console.log(`${firstArg} is ${secondArg}`);
