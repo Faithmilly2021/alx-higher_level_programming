09f80edac1b8f7b74125897caa76f3786d46ff63
Faith <faithmncube48@gmail.com>
2022-03-17 03:18:00 -0700

    methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Display all HTTP methods a server will accept for a given URL.
-curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
+curl -sI -X OPTIONS "$1" | grep Allow | cut -d ' ' -f1 --complement
