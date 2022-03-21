59fc87dd2a538db41a3329bc15ef524a46ac20b6
Faith <faithmncube48@gmail.com>
2022-03-20 19:33:13 -0700

    github

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
-"""ALX School interview"""
-
+"""List 10 commits (from the most recent to oldest) of a given
+    repository
+    Usage:
+        ./100-github_commits.py <repo name> <owner name>
+"""
+import sys
 import requests
-from sys import argv
+
 
 if __name__ == "__main__":
-    i = 0
-    commits = 'https://api.github.com/repos/'\
-              + argv[2] + '/' + argv[1] + '/commits'
-    response = requests.get(commits)
-    if "json" not in response.headers.get('content-type'):
-        print("Not a valid JSON")
-    else:
-        response = response.json()
-        for commit in response:
-            if i > 9:
-                break
-            print(commit.get('sha') + ': ', end="")
-            print(commit.get('commit').get('author').get('name'))
-            i += 1
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+            sys.argv[2], sys.argv[1])
+
+    commits = requests.get(url).json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
