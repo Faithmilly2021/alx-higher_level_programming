6a9825e8459a361fd8b8cde2784d8dbf01358de0
Faith <faithmncube48@gmail.com>
2022-03-20 18:59:39 -0700

    error code

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
-"""display the body"""
-
+"""Sends a request to the URL and displays the body of the response
+    (decoded in utf-8)
+"""
+import sys
+import urllib.error
 import urllib.request
-from sys import argv
+
 
 if __name__ == "__main__":
+    input_url = sys.argv[1]
+
+    request = urllib.request.Request(input_url)
     try:
-        with urllib.request.urlopen(argv[1]) as url:
-            print(url.read().decode('utf-8'))
+        with urllib.request.urlopen(request) as response:
+            print(response.read().decode("utf-8"))
     except urllib.error.HTTPError as e:
-        error_code = str(e).split(' ')[2][:-1]
-        print("Error code: " + str(error_code))
+        print("Error code: {}".format(e.code))
