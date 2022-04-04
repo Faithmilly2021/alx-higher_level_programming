b4463e9b272d2cbd00abe05fc2ddc93186e01a39
Faith Ncube <88321504+Faithmilly2021@users.noreply.github.com>
2022-04-04 08:31:30 +0200

    text

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,18 +1,28 @@
 #!/usr/bin/python3
-"""Text indentations"""
+"""
+Module text_indentation
+text_indentation
+"""
 
 
 def text_indentation(text):
-    """Prints a text with 2 new lines
-    Args:
-        text (string): The text to print.
-    Raises:
-        TypeError: If text is not a string.
     """
-    special = ['.', '?', ':']
-    if type(text) != str:
-        raise TypeError("text must be string")
-    for x in text:
-        print(x, end='')
-        if x in special:
-            print('\n\n', end='')
+       Prints indent text
+    """
+
+    if type(text) != str or text is None or len(text) < 0:
+        raise TypeError("text must be a string")
+    aux = 0
+    for c in text:
+        if c == '?' or c == ':' or c == '.':
+            print(c, end="\n\n")
+            aux = 1
+        else:
+            if aux == 0:
+                print(c, end="")
+            else:
+                if c == ' ' or c == '\t':
+                    pass
+                else:
+                    print(c, end="")
+                    aux = 0
